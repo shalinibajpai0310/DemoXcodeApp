2a775804c43a79f2f5b75363fe2b0199ff2f4acf
Ajay Girolkar <ajaygirolkar@gmail.com>
2024-01-10 21:38:15 +0530

    Initial Commit

---
+++ DemoXcodeApp/DemoXcodeApp/DemoXcodeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoXcodeAppApp.swift
+//  DemoXcodeApp
+//
+//  Created by Ajay Girolkar on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoXcodeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
